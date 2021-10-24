f877e62ede9120e1f5bd5253703123f3d963df68
RustemGaniev <ganievrr@gmail.com>
2021-10-24 12:38:42 +0300

    Functional

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,16 +7,18 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-
+// Пример аборигенского текста:
         String foreignText = "hate how we breathe and how we choke " +
                 "the night is black your heart is zero broke" +
                 "all into shapes and into shards" +
                 "we smash like glass we fall like star ";
-        List<String> dict = Arrays.asList(foreignText.split(" "));
+        // реализация монад
+        List<String> dict = Arrays.asList(foreignText.split(" ")); // функция высших порядков
         dict = dict.stream()
                 .sorted()
                 .distinct()
                 .collect(Collectors.toList());
+        // далее не отночится к задаче, просто тест для себя
         System.out.println(dict);
     }
 }
